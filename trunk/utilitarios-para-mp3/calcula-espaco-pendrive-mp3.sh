691e902f46686fd58de7b97ec8859bc8efd0d0ce
amg1127 <amg1127@5465fe1f-e9f4-4a51-a9b0-42098c041c11>
2011-01-30 16:29:29 +0000

    Adicionado script que calcula espaco disponivel em pendrive para a copia de MP3s.

---
+++ trunk/utilitarios-para-mp3/calcula-espaco-pendrive-mp3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if ! [ "x$2" != "x" ]; then
+    echo "Uso: $0 <caminho do pendrive> <itens> ..."
+    exit 1
+fi
+fs="$1"
+shift
+freespc=$((`stat -f -c '%a*%S' "$fs"`))
+usedspc="`du -bc \"$@\" | tail --lines=1 | awk '{ print $1; }'`"
+echo ' **** Espaco disponivel: ****'
+resp=$((freespc-usedspc))
+echo $resp bytes
+resp=$((resp/1024))
+echo $resp KB
+resp=$((resp/1024))
+echo $resp MB
+resp=$((resp/1024))
+echo $resp GB
